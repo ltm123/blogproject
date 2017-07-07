c87103c4d69755ad521e35ea3f3ab7ba1e323c2c
ltm123 <woaidyue@163.com>
2017-07-07 17:29:23 +0800

    first

---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Category, Tag, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
